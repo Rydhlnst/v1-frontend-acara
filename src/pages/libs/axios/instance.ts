22d147bb4f1e6d3a48df5dab153b93f3e4f0f1c4
Rydhlnst <66582308+Rydhlnst@users.noreply.github.com>
2025-02-19 17:59:17 +0700

    axios fix

--- src/pages/libs/axios/instance.ts
+++ src/pages/libs/axios/instance.ts
@@ -1,39 +1,37 @@
-import { environment } from "@/config/environment";
-import axios from "axios";
-import { Session } from "next-auth";
-import { getSession } from "next-auth/react";
+
+import { environment } from '@/config/environment';
+import axios from 'axios';
+import { Session } from 'next-auth';
+import { getSession } from 'next-auth/react';
 
 interface CustomSession extends Session {
-    accessToken?: string
+  accessToken?: string;
 }
 
 const headers = {
-    "Content-Type": "application/json"
-}
+  'Content-Type': 'application/json',
+};
 
-// Membuat axios instance
 const instance = axios.create({
-    baseURL: environment.API_URL,
-    headers,
-    timeout: 60 * 1000
+  baseURL: environment.API_URL,
+  headers,
+  timeout: 60 * 1000,
 });
 
-// Kenapa menggunakan interceptors? Karena untuk mengecek error dan session Token Request ke dalam API 
 instance.interceptors.request.use(
-    async (request) => {
-        // Mengecek akses token untuk login
-        const session: CustomSession | null = await getSession();
-        if(session && session.accessToken) {
-            request.headers.Authorization = `Bearer ${session.accessToken}`
-        }
-        return request;
-    },
-    (error) => Promise.reject(error)
+  async (request) => {
+    const session: CustomSession | null = await getSession();
+    if (session && session.accessToken) {
+      request.headers.Authorization = `Bearer ${session.accessToken}`;
+    }
+    return request;
+  },
+  (error) => Promise.reject(error),
 );
 
 instance.interceptors.response.use(
-    (response) => response,
-    (error) => Promise.reject(error)
-)
+  (response) => response,
+  (error) => Promise.reject(error),
+);
 
 export default instance;
