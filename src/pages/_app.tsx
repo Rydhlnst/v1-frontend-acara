f635ce495876444970f4f4ae34cb8d829dd84006
Rydhlnst <66582308+Rydhlnst@users.noreply.github.com>
2025-02-19 17:36:59 +0700

    Register Page UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,20 @@ import "@/styles/globals.css";
 import * as React from "react";
 import type { AppProps } from "next/app";
 import { NextUIProvider } from "@nextui-org/react";
+import { Inter } from "next/font/google";
+import { cn } from "@/utils/cn";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
-      <Component {...pageProps} />;
+      <main className={cn(inter.className, "flex min-h-screen flex-col items-center justify-center gap-10 py-10 lg:py-0")}>
+        <Component {...pageProps} />;
+      </main>
     </NextUIProvider>
 
   )
---
+++ src/pages/auth/register/index.tsx
@@ -0,0 +1,14 @@
+// Menggunakan folder pages untuk routing URL
+
+import AuthLayout from "@/components/layouts/AuthLayout";
+import Register from "@/components/views/Register";
+
+const RegisterPage = () => {
+  return (
+    <AuthLayout title="Acara | Register">
+        <Register />
+    </AuthLayout>
+  )
+}
+
+export default RegisterPage;
